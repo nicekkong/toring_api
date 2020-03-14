60cf702ca600bf3e5f47246791f22590fc82baea
nicekkong <nicekkong@gmail.com>
2020-03-15 01:25:26 +0900

    테스트 코드 삭제

--- src/test/java/com/cplanet/toring/ToringApplicationTests.java
+++ src/test/java/com/cplanet/toring/ToringApplicationTests.java
@@ -1,11 +1,8 @@
 package com.cplanet.toring;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
-
-import java.util.List;
 
 @SpringBootTest
 @Profile("local")
@@ -14,17 +11,4 @@ class ToringApplicationTests {
     @Test
     void contextLoads() {
     }
-
-
-    @Value("${login.url.patterns}")
-    List<String> loginUrls;
-
-    @Test
-    public void testP() {
-
-        loginUrls.forEach(System.out::println);
-
-    }
-
-
 }

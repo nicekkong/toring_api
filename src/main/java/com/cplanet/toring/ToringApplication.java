bd0f2513ccf89fb5ec04e78f03ecf37c6aad041d
nicekkong <nicekkong@gmail.com>
2020-03-15 15:25:17 +0900

    소스 Commit

--- src/main/java/com/cplanet/toring/ToringApplication.java
+++ src/main/java/com/cplanet/toring/ToringApplication.java
@@ -2,13 +2,20 @@ package com.cplanet.toring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class ToringApplication {
+public class ToringApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ToringApplication.class, args);
+    }
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)
+    {
+        return builder.sources(ToringApplication.class);
     }
 }

f18cdc08e2a2082ec8f63d83a1b27412e765b00a
nicekkong <nicekkong@gmail.com>
2020-03-15 14:36:18 +0900

    Application Override

--- src/main/java/com/cplanet/toring/ToringApplication.java
+++ src/main/java/com/cplanet/toring/ToringApplication.java
@@ -1,15 +1,22 @@
 package com.cplanet.toring;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class ToringApplication {
+public class ToringApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ToringApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
 }

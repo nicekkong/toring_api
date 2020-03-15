8a9c658cf4a84e0b9d017a0c78a2a6f5c958ec3a
nicekkong <nicekkong@gmail.com>
2020-03-15 16:25:36 +0900

    RestTemplate sample contorller

--- src/main/java/com/cplanet/toring/controller/SampleRestController.java
+++ src/main/java/com/cplanet/toring/controller/SampleRestController.java
@@ -1,26 +1,40 @@
 package com.cplanet.toring.controller;
 
 import com.cplanet.toring.domain.TestData;
+import com.cplanet.toring.dto.MaskResponseDto;
 import com.cplanet.toring.dto.TestDataResponseDto;
 import com.cplanet.toring.repository.TestDataRepository;
 import com.cplanet.toring.service.TestDataService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class SampleRestController {
 
+    private final static Logger logger = LoggerFactory.getLogger(SampleRestController.class);
+
 
     final private TestDataService testDataService;
     final private TestDataRepository testDataRepository;
+    final private RestTemplate restTemplate;
 
-    public SampleRestController(TestDataService testDataService, TestDataRepository testDataRepository) {
+    public SampleRestController(TestDataService testDataService
+                                ,TestDataRepository testDataRepository
+                                ,RestTemplate restTemplate
+    ) {
         this.testDataService = testDataService;
         this.testDataRepository = testDataRepository;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping(value = "/time")
@@ -36,9 +50,27 @@ public class SampleRestController {
 
 
     @GetMapping(value = "/data2")
-    public List<TestData> data2() {
+    public List<TestDataResponseDto> data2() {
         List<TestData> result = testDataRepository.findAll();
-        return result;
+
+        return result.stream().map(TestDataResponseDto::new).collect(Collectors.toList());
+
+    }
+
+    @GetMapping(value="mask")
+    public MaskResponseDto mask() {
+        HashMap<String, String> param = new  HashMap<>();
+        param.put("address", "서울특별시 서초구 잠원동");
+
+//        MaskResponseDto response = restTemplate.getForObject("https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByAddr/json?address={address}", MaskResponseDto.class,  param);
+
+        ResponseEntity<MaskResponseDto> responseEntity = restTemplate.getForEntity("https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByAddr/json?address={address}", MaskResponseDto.class,  param);
+
+        logger.debug("result => {}", responseEntity.getBody());
+
+        return responseEntity.getBody();
+
+
     }
 
 }

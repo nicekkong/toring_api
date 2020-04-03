5081110494b4a39ea3a3cd98c3dd144e70360cbc
권범직(Bumjick Kwon)/OCB개발팀/SKP <bumziki@sk.com>
2020-04-03 21:03:31 +0900

    S3 File Upload 구현

---
+++ src/main/java/com/cplanet/toring/controller/FileUploadController.java
@@ -0,0 +1,43 @@
+package com.cplanet.toring.controller;
+
+import com.cplanet.toring.dto.FileUploadResponseDto;
+import com.cplanet.toring.service.S3Service;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/file")
+public class FileUploadController {
+    private S3Service s3Service;
+
+    public FileUploadController(S3Service s3Service) {
+        this.s3Service = s3Service;
+    }
+
+    @PostMapping("/upload")
+    public FileUploadResponseDto uploadFile(MultipartFile file) {
+        FileUploadResponseDto response = new FileUploadResponseDto();
+        try {
+            String filePath = s3Service.upload(file);
+            if(!StringUtils.isEmpty(filePath)) {
+                response.setSuccess(true);
+                response.setFilepath(filePath);
+            } else {
+                response.setSuccess(false);
+            }
+        } catch (IOException e) {
+            log.error("file upload error", e);
+            response.setSuccess(false);
+        }
+        return response;
+    }
+
+}
---
+++ src/main/java/com/cplanet/toring/dto/FileUploadResponseDto.java
@@ -0,0 +1,9 @@
+package com.cplanet.toring.dto;
+
+import lombok.Data;
+
+@Data
+public class FileUploadResponseDto {
+    private boolean success;
+    private String filepath;
+}

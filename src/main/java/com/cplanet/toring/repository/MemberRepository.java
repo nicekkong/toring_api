c0978288ae796b1a2c7e8b616a484fdf7e886a5a
권범직/OCB개발팀 <bumziki@sk.com>
2020-05-24 22:18:38 +0900

    MemberRepository. @Param 추가

--- src/main/java/com/cplanet/toring/repository/MemberRepository.java
+++ src/main/java/com/cplanet/toring/repository/MemberRepository.java
@@ -3,14 +3,15 @@ package com.cplanet.toring.repository;
 import com.cplanet.toring.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findById(Long id);
 
-    @Query(value = "select m from Member m join fetch m.profile p where m.id=:id")
-    Optional<Member> findByIdWithProfile(Long id);
+    @Query(value = "select m from Member m left join m.profile p where m.id=:id")
+    Optional<Member> findByIdWithProfile(@Param(value="id")Long id);
 
     Optional<Member> findByEmail(String email);
 

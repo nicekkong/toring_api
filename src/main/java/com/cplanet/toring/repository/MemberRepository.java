329576af5fa723aec0d3784c017e9cfa4f7ee796
권범직/OCB개발팀 <bumziki@sk.com>
2020-05-27 06:10:09 +0900

    MemberRepository. @Param 제거 후 참조값 ?1 로 변경

--- src/main/java/com/cplanet/toring/repository/MemberRepository.java
+++ src/main/java/com/cplanet/toring/repository/MemberRepository.java
@@ -3,15 +3,14 @@ package com.cplanet.toring.repository;
 import com.cplanet.toring.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findById(Long id);
 
-    @Query(value = "select m from Member m left join m.profile p where m.id=:id")
-    Optional<Member> findByIdWithProfile(@Param(value="id") Long id);
+    @Query(value = "select m from Member m left join m.profile p where m.id=?1")
+    Optional<Member> findByIdWithProfile(Long id);
 
     Optional<Member> findByEmail(String email);
 
